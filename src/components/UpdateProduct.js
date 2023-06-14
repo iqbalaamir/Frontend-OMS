652c80180bb80df9cb4faf240c31e390fca136a4
iqbalaamir <aamir.iqbal040@gmail.com>
2023-06-15 00:51:52 +0530

    final

--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -22,7 +22,7 @@ export default function UpdateProduct({
   };
 
   const updateProduct = () => {
-    fetch("http://localhost:4000/api/product/update", {
+    fetch("https://order-management-api.onrender.com/api/product/update", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -21,7 +21,7 @@ function Register() {
 
   // Register User
   const registerUser = () => {
-    fetch("http://localhost:4000/api/register", {
+    fetch("https://order-management-api.onrender.com/api/register", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
